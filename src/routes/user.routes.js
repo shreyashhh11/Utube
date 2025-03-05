6e0e6c4de0ca6cd9af04e748144378c731ab57eb
risus11 <shreyash6432@gmail.com>
2025-03-06 01:00:16 +0530

    lil

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -30,7 +30,7 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").post(verifyJWT, getCurrentUser)
 router.route("/update-account-details").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("   coverImage"), updateUserCoverImage)
 router.route("/c/:username").get (verifyJWT, getUserChannelProfile)
 router.route("/watch-History").get(verifyJWT, getWatchHistory)
 export default router
